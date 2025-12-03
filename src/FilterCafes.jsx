fcae823e40b0cf674526ea006acedcc906c40265
Alexei Zaikin <zaikinaleks04@mail.ru>
2025-12-03 14:10:49 +0300

    added generate adresses for filter

--- src/FilterCafes.jsx
+++ src/FilterCafes.jsx
@@ -23,15 +23,15 @@ const dataForFilter = [
 ];
 
 export default function FilterCafes() {
-    
+
     return (
         <select name="subway" id="subway">
 	        <option value="All">Все</option>
-            <option value="Московская">Московская</option>
-			<option value="Арбат">Арбат</option>
-		    <option value="Парк Культуры">Парк Культуры</option>
-			<option value="Красная Площадь">Красная Площадь</option>
-			<option value="Театральная">Театральная</option>
+            {dataForFilter.map(item => (
+                <option key={item.code} value={item.code}>
+                    {item.name}
+                </option>
+            ))}
         </select>
     )
 }
